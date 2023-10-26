f005cca09128152d36b30a44618089c55995510c
Gustavo Luchetti <gustavo@luchetti.dev>
2023-10-26 16:48:58 -0300

    bug: pushed bug to fix live

--- fib.js
+++ fib.js
@@ -1,10 +1,12 @@
-console.log(fib(16));
+// indice 9, queremos que o resultado seja 34!
+console.log(fib(9));
 
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
 function fib(pos) {
-  if (pos == 1 || pos == 0) {
+  if (pos == 0 || pos == 1) {
     return pos;
   } else {
-    let result = fib(pos - 2) + fib(pos - 1);
+    let result = fib(pos - 3) + fib(pos - 1);
     return result;
   }
 }
